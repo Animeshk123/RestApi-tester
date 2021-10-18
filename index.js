b9fa5d41b79ffa77ae9b449e9cc6667d2669da22
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2021-10-18 20:17:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+
+const tryNOw = document.querySelector(".button");
+const Send = document.querySelector(".requerstBox");
+const header = document.querySelector(".header");
+const main = document.querySelector(".main");
+const inputBox = document.querySelector(".apiBox");
+const resultBox = document.querySelector(".results");
+async function getData () {
+    const data = await fetch("users.json");
+    const res = await data.json();
+    const textarea = document.createElement("textarea");
+    textarea.classList.add("pretag");
+    textarea.disabled = true;
+    resultBox.appendChild(textarea);
+    textarea.value = JSON.stringify(res,null,4);
+}
+
+getData();
+
+
+tryNOw.addEventListener("click", () => {
+    header.classList.add("headeradd");
+    setTimeout(() => {
+        header.style.display = "none";
+    }, 1000);
+    setTimeout(() => {
+        main.classList.add("mainadd");
+    },1000);
+
+});
+
+Send.addEventListener("click",async () => {
+    if(inputBox.value === "") {
+      resultBox.innerHTML = `<h4>error invalid url</h4>`;
+    }
+    else {
+      resultBox.innerHTML = "<textarea class='pretag' disabled>" +"loading please wait ..."+ "</textarea>";
+      try{
+      const data = await fetch(inputBox.value);
+      const res = await data.json();
+      resultBox.innerHTML = "<textarea class='pretag' disabled>" + JSON.stringify(res,null,4) + "</textarea>";
+      }
+      catch(err) {
+        resultBox.innerHTML = `<h4>error invalid url</h4>`;
+      }
+    }
+})
